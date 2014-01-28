124a7581fa2d833f41f411e9e7476c9a150087df
tsundere-cafe <kaypav@gmail.com>
2014-01-28 14:19:54 -0700

    add unity project

---
+++ WaitForEvents/Assets/Scripts/Receives2DCollisionEvents.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using Require;
+
+public class Receives2DCollisionEvents : MonoBehaviour
+{
+    public float radius;
+    public LayerMask mask;
+
+    public WaitFor<Transform> waitForCollision = new WaitFor<Transform>();
+
+    void FixedUpdate()
+    {
+        foreach (Collider2D other in Physics2D.OverlapCircleAll(transform.position, radius, mask))
+        {
+            waitForCollision.Happened(other.transform);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
---
+++ WaitForEvents/Assets/Scripts/WaitFor.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System;
+
+public class WaitFor
+{
+    List<Action> thens = new List<Action>();
+    List<Action> thenAlwayses = new List<Action>();
+
+    public void Then(Action action)
+    {
+        thens.Add(action);
+    }
+
+    public void ThenAlways(Action action)
+    {
+        thenAlwayses.Add(action);
+    }
+
+    public void Flush()
+    {
+        thens = new List<Action>();
+        thenAlwayses = new List<Action>();
+    }
+
+    public void Happened()
+    {
+        foreach (Action action in thens)
+        {
+            action.Invoke();
+        }
+
+        foreach (Action action in thenAlwayses)
+        {
+            action.Invoke();
+        }
+
+        thens = new List<Action>();
+    }
+}
+
+public class WaitFor<T>
+{
+    List<Action<T>> thens = new List<Action<T>>();
+    List<Action<T>> thenAlwayses = new List<Action<T>>();
+
+    public void Then(Action<T> action)
+    {
+        thens.Add(action);
+    }
+
+    public void ThenAlways(Action<T> action)
+    {
+        thenAlwayses.Add(action);
+    }
+
+    public void Happened(T arg)
+    {
+        foreach (Action<T> action in thens)
+        {
+            action.Invoke(arg);
+        }
+
+        foreach (Action<T> action in thenAlwayses)
+        {
+            action.Invoke(arg);
+        }
+
+        thens = new List<Action<T>>();
+    }
+}
