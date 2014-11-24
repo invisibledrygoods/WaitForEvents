997da927486232bd3ad6a71b1af922a944e1e34c
= <olsenjeffn@gmail.com>
2014-11-23 18:02:15 -0700

    add IsDestroyable and Receives3DCollisionEvents

---
+++ WaitForEvents/Assets/Scripts/IsDestroyable.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using Require;
+
+public class IsDestroyable : MonoBehaviour
+{
+    public WaitFor waitForDestroyed = new WaitFor();
+
+    public void Destroy()
+    {
+        waitForDestroyed.Happened();
+        Destroy(gameObject);
+    }
+}
---
+++ WaitForEvents/Assets/Scripts/Receives3DCollisionEvents.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using Require;
+
+public class Receives3DCollisionEvents : MonoBehaviour
+{
+    public float radius;
+    public LayerMask mask;
+
+    public WaitFor<Transform> waitForCollision = new WaitFor<Transform>();
+
+    void FixedUpdate()
+    {
+        foreach (Collider other in Physics.OverlapSphere(transform.position, radius, mask))
+        {
+            waitForCollision.Happened(other.transform);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
